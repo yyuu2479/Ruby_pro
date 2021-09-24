baa7edafbd7b3b37c8c7e952facb4eea57317bea
yyuu2479 <yyuu2479@gmail.com>
2021-09-24 11:59:42 +0900

    Ruby_pro

--- FizzBuzz/FizzBuzz.rb
+++ FizzBuzz/FizzBuzz.rb
@@ -1,3 +1,19 @@
-def method_name
-  puts "こんにちは"
+def fizz_buzz(number)
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  else
+    puts number
+  end
 end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(15)
