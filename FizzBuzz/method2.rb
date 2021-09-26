889cfeb6f8cf58a136761b93f19fdf2fa162b149
yyuu2479 <yyuu2479@gmail.com>
2021-09-26 23:20:16 +0900

    Ruby_pro

--- FizzBuzz/method2.rb
+++ FizzBuzz/method2.rb
@@ -18,9 +18,37 @@ p default(1, 2, 3, 4)
 def foo(time = Time.now, message = bar)
   puts "time: #{time}, message: #{message}"
 end
-
 def bar
   'BAR'
 end
-
 foo
+
+p ''.empty?
+p 'a'.empty?
+
+p ''.include?('a')
+p 'a'.include?('a')
+
+p 1.odd?
+p 2.odd?
+
+p 1.even?
+p 2.even?
+
+p nil.nil?
+
+
+a = "ruby"
+p a.upcase
+p a
+
+p a.upcase!
+p a
+
+def reverse_upcase!(s)
+  s.reverse!.upcase!
+end
+
+s = "ruby"
+p reverse_upcase!(s)
+p s
