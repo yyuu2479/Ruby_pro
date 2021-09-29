78ea61579782348d671bce31013ea7516ff20ecc
yyuu2479 <yyuu2479@gmail.com>
2021-09-29 15:33:55 +0900

    Ruby_pro_book

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -6,14 +6,6 @@ def fizz_buzz(number)
   elsif number % 3 == 0
     "Fizz"
   else
-    number
+    number.to_s
   end
 end
-
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
--- minitest/test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-
-class SampleTest < Minitest::Test
-  def test_sample
-    assert_equal 'RUBY', 'ruby'.upcase
-    assert true
-  end
-end
