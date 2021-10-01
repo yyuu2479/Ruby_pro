9627dc6f2296bbc9fc0eeb853c5e7c2a3819c9d8
yyuu2479 <yyuu2479@gmail.com>
2021-10-01 23:58:18 +0900

    Ruby_pro_book

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -10,5 +10,6 @@ class FizzBuzzTest < Minitest::Test
     assert_equal 'Buzz', fizz_buzz(5)
     assert_equal 'Fizz', fizz_buzz(6)
     assert_equal 'FizzBuzz', fizz_buzz(15)
+    assert_equal 'FizzBuzz', fizz_buzz(30)
   end
 end
