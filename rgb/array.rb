d4ac24f33500417b9aa127e35d74ad18bf2a9813
yyuu2479 <yyuu2479@gmail.com>
2021-10-27 21:17:33 +0900

    [Add]array

--- rgb/array.rb
+++ rgb/array.rb
@@ -1,17 +1,85 @@
-p [].class
+# quotient, remainder = 14.divmod(3)
+# puts "商は#{quotient}, 余りは#{remainder}"
 
-a = [1,2,3]
-a[1] = 20
-a[4] = 30
-a << 5
-p a.delete_at(1)
-p a
+# numbers = [1,2,3,4]
+# sum = 0
+# numbers.each do |number|
+#   sum_value = number.even? ? number * 10 : number
+#   sum += sum_value
+# end
+# p sum
 
-p b = ['apple', 'orange', 'melon']
+# a = [1,2,3,1,2,3]
+# a.delete_if { |n| n.odd? }
+# p a
 
-p c = ['apple', 1, 'orange', 2, 'melon', 3]
+# p b = [1,2,3,4,5,6]
+# c = b.map {|n| n * 10}
+# p c
 
-p d = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+# even_number = numbers.select { |number| number.even? }
+# p even_number
 
-quotient, remainder = 14.divmod(3)
-puts "商は#{quotient}, 余りは#{remainder}"
+# odd_number = numbers.reject { |number| number.even? }
+# p odd_number
+
+# sum = numbers.inject(0) { |result, n| result + n }
+# p sum
+
+# weeks = ["mon", "tue", "wed", "thu", "fri", "sat"]
+# day_week = weeks.inject("sun") { |week, s| week + s }
+# p day_week
+
+# p ["ruby", "java", "php"].map { |s| s.upcase }
+# p ["ruby", "java", "php"].map(&:upcase)
+
+# p [1,2,3,4,5,6].select { |n| n.odd? }
+# p [1,2,3,4,5,6].select(&:odd?)
+
+# p [9, 10, 11, 12].map {|n| n.to_s(16)}
+
+# a = [1,2,3,4,5]
+# p a[1..3]
+
+# s = "abcdefg"
+# p s[1..3]
+
+# def liquid?(temperature)
+#   (0...100).include?(temperature)
+# end
+# p liquid?(-1)
+# p liquid?(50)
+# p liquid?(100)
+
+# def charge(age)
+#   case age
+#     when 0..5
+#       0
+#     when 6..12
+#       300
+#     when 13..18
+#       600
+#     else
+#       1000
+#   end
+# end
+# p charge(3)
+# p charge(12)
+# p charge(16)
+# p charge(25)
+
+# p (1..5).to_a
+# p (1...5).to_a
+
+# p ("a".."e").to_a
+# p ("a"..."e").to_a
+
+# p [*1..5].to_a
+
+sum = 0
+(1..4).each { |n| sum += n }
+p sum
+
+numbers = []
+(1..10).step(2) { |n| numbers << n }
+p numbers
