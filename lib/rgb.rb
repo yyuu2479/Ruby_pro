0027deeec61412ad2cd3cd3f5d1c7c0f928300be
yyuu2479 <yyuu2479@gmail.com>
2021-10-27 22:40:45 +0900

    [Add]array

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,3 +1,10 @@
 def to_hex(r, g, b)
   [r,g,b].inject("#") { |hex, n| hex + n.to_s(16).rjust(2, "0") }
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r, g, b].map { |s| s.hex }
+end
