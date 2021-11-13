9de99c9314a4c60b0f9d2783656c60e85c71afb1
yyuu2479 <yyuu2479@gmail.com>
2021-11-13 15:58:22 +0900

    ruby_class

--- Class/class.rb
+++ Class/class.rb
@@ -79,23 +79,66 @@
 #   SOME_NAMES = ["Foo", "Bar", "Baz"].map(&:freeze).freeze
 # end
 
-class User
-  attr_reader :name, :weight
+# class User
+#   attr_reader :name, :weight
 
-  def initialize(name, weight)
+#   def initialize(name, weight)
+#     @name = name
+#     @weight = weight
+#   end
+
+#   def heavier_than?(other_user)
+#     other_user.weight < @weight
+#   end
+
+#   protected :weight
+# end
+
+
+class Product
+  attr_reader :name
+
+  DEFAULT_PRICE = 0
+  NAME = "product"
+  SOME_NAMES = ["Foo", "Bar", "Baz"].map(&:freeze).freeze
+
+  @name = "Product"
+
+  def initialize(name)
     @name = name
-    @weight = weight
   end
 
-  def heavier_than?(other_user)
-    other_user.weight < @weight
+  def self.name
+    @name
   end
 
-  protected :weight
+  def self.names_without_foo(names = SOME_NAMES)
+    names.delete('Foo')
+    names
+  end
 end
 
-alice = User.new("アリス", 45)
-bob = User.new("ボブ", 60)
+class DVD < Product
+  @name = "DVD"
 
-puts alice.heavier_than?(bob)
-puts bob.heavier_than?(alice)
+  def self.name
+    @name
+  end
+
+  def upcase_name
+    @name.upcase
+  end
+end
+
+puts Product.name
+puts DVD.name
+
+product = Product.new("A greet movie")
+puts product.name
+
+dvd = DVD.new("An awesome film")
+puts dvd.name
+puts dvd.upcase_name
+
+puts Product.name
+puts DVD.name
