fccbeb5be4cbc4a3490c57f0ed96edc7fe28b00f
yyuu2479 <yyuu2479@gmail.com>
2021-11-14 22:30:51 +0900

    ruby_class

--- Class/class1.rb
+++ Class/class1.rb
@@ -4,3 +4,31 @@ c = a
 
 p a.equal?(b)
 p a.equal?(c)
+
+# モンキーパッチ
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    "Hello, #{@name}"
+  end
+end
+
+user = User.new("アリス")
+p user.hello
+
+class User
+  # def hello
+  #   "#{@name}さん、こんにちは！！"
+  # end
+
+  alias hello_original hello
+
+  def hello
+    "#{hello_original}　じゃなくて、 #{@name}さん、こんにちは"
+  end
+end
+
+p user.hello
