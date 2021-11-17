9981c14fc4b99eafd1548a25553b24a5d8782a74
yyuu2479 <yyuu2479@gmail.com>
2021-11-17 15:35:58 +0900

    ruby_class

--- Class/class1.rb
+++ Class/class1.rb
@@ -1,46 +1,100 @@
-a = "abc"
-b = "def"
-c = a
+# a = "abc"
+# b = "def"
+# c = a
 
-p a.equal?(b)
-p a.equal?(c)
+# p a.equal?(b)
+# p a.equal?(c)
 
-# モンキーパッチ
-class User
-  def initialize(name)
+# # モンキーパッチ
+# class User
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def hello
+#     "Hello, #{@name}"
+#   end
+# end
+
+# class User
+#   # def hello
+#   #   "#{@name}さん、こんにちは！！"
+#   # end
+
+#   alias hello_original hello
+
+#   def hello
+#     "#{hello_original}　じゃなくて、 #{@name}さん、こんにちは"
+#   end
+# end
+
+
+# # クラスメゾット
+# class User
+# end
+
+# def User.hello
+#   "Hello!"
+# end
+
+# class << User
+#   def hi
+#     "Hi!"
+#   end
+# end
+
+# p User.hello
+# p User.hi
+
+# # 特異メゾット
+# alice = "I am alice."
+
+# def alice.hello
+#   "Hello."
+# end
+
+# class << alice
+#   def hi
+#     "Hi."
+#   end
+# end
+
+# ダックタイピング
+# def display_name(object)
+#   if object.respond_to?(:name)
+#     puts "Name is <<#{object.name}>>"
+#   else
+#     puts "No name."
+#   end
+# end
+
+# class User
+#   def name
+#     "Alice"
+#   end
+# end
+
+class Product
+  def initialize(name, price)
     @name = name
+    @price = price
   end
 
-  def hello
-    "Hello, #{@name}"
+  def stock?
+    raise "サブクラスでstock?メゾットを実装してください！"
   end
-end
 
-class User
-  # def hello
-  #   "#{@name}さん、こんにちは！！"
-  # end
-
-  alias hello_original hello
-
-  def hello
-    "#{hello_original}　じゃなくて、 #{@name}さん、こんにちは"
+  def display_text
+    stock = stock? ? "あり" : "なし"
+    "商品名: #{@name} 価格: #{@price} 在庫: #{stock}"
   end
 end
 
-
-# 特異メゾット
-alice = "alice"
-bob = "bob"
-
-def alice.shuffle
-  chars.shuffle.join
-end
-p alice.shuffle
-
-class << alice
-  def shuffle
-    chars.shuffle.join
+class DVD
+  def stock?
+    true
   end
 end
-p alice.shuffle
+
+dvd = DVD.new
+p dvd.is_a?(DVD)
